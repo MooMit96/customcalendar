4100ae7f7121d4ed26fe2dfa930cc90ead0f3d9f
smilekbear <smilkebear@gmail.com>
2023-03-07 15:01:26 +0900

    test

--- customcalendar/src/main/java/kr/co/mit/customcalendar/DecoratorResult.java
+++ customcalendar/src/main/java/kr/co/mit/customcalendar/DecoratorResult.java
@@ -5,7 +5,7 @@ package kr.co.mit.customcalendar;
 public class DecoratorResult {
     public final DayViewDecorator decorator;
     public final DayViewFacade result;
-
+//tt
     DecoratorResult(DayViewDecorator decorator, DayViewFacade result) {
         this.decorator = decorator;
         this.result = result;
